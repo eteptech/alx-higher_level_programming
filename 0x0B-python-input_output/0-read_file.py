c56a634d7cb8fa1391b9eaf95f6d4d112b836094
emmanuel Ugbem <ugbememmanuel4@gmail.com>
2023-05-30 10:33:56 +0000

    initial commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-filename = input("Select a file:")
 
-with open(filename, 'r', encoding = 'utf-8') as f:
-    output =  f.read()
-    print(output)
+def readFile(filename):
+    with open(filename,encoding = 'utf-8') as f:
+        output =  f.read()
+        print(output)
 
