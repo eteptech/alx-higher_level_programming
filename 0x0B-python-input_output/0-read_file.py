2edecb0dc7f5a802549b3425c197486fd35868bb
emmanuel Ugbem <ugbememmanuel4@gmail.com>
2023-05-30 10:53:07 +0000

    corrected

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
 
 
-def readFile(filename):
-
-    with open(filename, 'r', encoding = 'utf-8') as f:
-        output =  f.read()
-        print(output)
-readFile("README.md")
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
